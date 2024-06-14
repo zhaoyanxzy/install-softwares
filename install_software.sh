d854a0d2a1222f8b0c98dacd47a3d38b25bb6a7e
zhaoyanxzy <48901763+zhaoyanxzy@users.noreply.github.com>
2024-06-14 10:17:32 +0800

    Update install_software.sh

--- install_software.sh
+++ install_software.sh
@@ -5,6 +5,8 @@ if ! command -v brew &> /dev/null
 then
     echo "Homebrew not found. Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/zhaoyan.x/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "Homebrew already installed."
 fi
